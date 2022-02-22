cfba1ed2729e0808fb84f290b91f8c94a5ad4884
CAPSLOCKFURY <78479907+CAPSLOCKFURY@users.noreply.github.com>
2022-02-22 22:48:16 +0200

    Add files via upload

---
+++ src/main/java/ua/kpi/tef/util/UserMealsUtil.java
@@ -0,0 +1,42 @@
+package ua.kpi.tef.util;
+
+import ua.kpi.tef.model.UserMeal;
+import ua.kpi.tef.model.UserMealWithExceed;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.*;
+
+/**
+ * GKislin
+ * 31.05.2015.
+ */
+public class UserMealsUtil {
+    public static void main(String[] args) {
+        List<UserMeal> mealList = Arrays.asList(
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+        );
+        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
+//        .toLocalDate();
+//        .toLocalTime();
+    }
+
+    public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+        Map<Integer, Integer> dayMap = new HashMap<>();
+        for(UserMeal meal : mealList){
+            dayMap.merge(meal.getDateTime().getDayOfMonth(), meal.getCalories(), Integer::sum);
+        }
+        List<UserMealWithExceed> result = new LinkedList<>();
+        for(UserMeal meal: mealList){
+            boolean exceeded = dayMap.get(meal.getDateTime().getDayOfMonth()) > caloriesPerDay;
+            result.add(new UserMealWithExceed(meal.getDateTime(), meal.getDescription(), meal.getCalories(), exceeded));
+        }
+        return result;
+    }
+}
